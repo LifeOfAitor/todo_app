d432de0ee3bd1a37d4a00ad91d95145343242e88
LifeOfAitor <aitorgaillard99@gmail.com>
2024-07-27 11:43:35 +0200

    final touches, add clock and theme

--- gui.py
+++ gui.py
@@ -1,31 +1,37 @@
 import FreeSimpleGUI as sg
 import functions
+import time
 
+sg.theme("DarkTeal9")
 todos = functions.loadtodos()
 
-label = sg.Text("Tareas: ")
-input_box = sg.InputText(key="todo")
+clock = sg.Text("", key="clock")
+input_box = sg.InputText(key="todo", size=(40, 1))
 add_button = sg.Button("Añadir")
 
 listbox = sg.Listbox(todos,
                      key="todos",
                      enable_events=True,
-                     size=(60, 10))
+                     size=(40, 10))
 
 edit_button = sg.Button("Editar")
 
 complete_button = sg.Button("Completar")
 exit_button = sg.Button("Salir")
 
-layout = [[label, input_box, add_button],
+layout = [[clock],
+          [input_box, add_button],
           [listbox],
           [edit_button, complete_button, exit_button]]
 
 window = sg.Window("TODO_APP", layout)
+
 while True:
-    # functions.showresults(todos)
-    event, values = window.read()
-    # print(event, values)
+    event, values = window.read(timeout=1000)
+    if event in (sg.WIN_CLOSED, "Salir"):
+        break
+    # Update the clock
+    window["clock"].update(time.strftime("%m-%d  %H:%M"))
     match event:
         case sg.WIN_CLOSED:
             break
@@ -33,20 +39,23 @@ while True:
             todos.append(values["todo"])
             functions.writefile(todos)
             window["todos"].update(todos)
-        case "Editar":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
-            functions.tareamodify(todo_to_edit, new_todo, todos)
-            window["todos"].update(todos)
-        case "Completar":
-            todo_to_complete = values["todos"][0]
-            print(todo_to_complete)
-            functions.tareacompletar(todo_to_complete, todos)
-            print(todos)
-            window["todos"].update(todos)
             window["todo"].update("")
-        case "Salir":
-            break
+        case "Editar":
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+                functions.tareamodify(todo_to_edit, new_todo, todos)
+                window["todos"].update(todos)
+            except IndexError:
+                sg.popup("Elige una tarea de la lista")
+        case "Completar":
+            try:
+                todo_to_complete = values["todos"][0]
+                functions.tareacompletar(todo_to_complete, todos)
+                window["todos"].update(todos)
+                window["todo"].update("")
+            except IndexError:
+                sg.popup("Elige una tarea de la lista")
         case "todos":
             # actualizar la lista en la ventana
             window["todo"].update(values["todos"][0])
